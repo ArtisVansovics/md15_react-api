d32c453a72a483deb23190c839380c4815a446f4
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-12 18:22:07 +0300

    Added basic pagination to Characters Page

--- src/Pages/Characters/CharactersPage.tsx
+++ src/Pages/Characters/CharactersPage.tsx
@@ -1,44 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Character } from '../../Models/CharacterModel';
+import { Info } from '../../Models/InfoModel';
 import Button from '../../components/Button/Button';
 import CharacterCard from '../../components/CharacterCard/CharacterCard';
 import Loader from '../../components/Loader/Loader';
 
 const CharactersPage = () => {
   const [characters, setCharacters] = useState<Character[]>();
+  const [info, setInfo] = useState<Info>();
   const [filteredCharacters, setFilteredCharacters] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageCount, setPageCount] = useState<number>();
+  // const [visiblePages, setVisiblePages] = useState<number[]>();
 
   const buttons = [
     {
       title: 'All',
       bgColor: '#11AEBF',
-      onClick: () => setFilteredCharacters(''),
+      onClick: () => {
+        setFilteredCharacters('');
+        setCurrentPage(1);
+      },
     },
     {
       title: 'Alive',
       bgColor: '#BFD962',
-      onClick: () => setFilteredCharacters('?status=alive'),
+      onClick: () => {
+        setFilteredCharacters('alive');
+        setCurrentPage(1);
+      },
     },
     {
       title: 'Dead',
       bgColor: '#D94E4E',
-      onClick: () => setFilteredCharacters('?status=dead'),
+      onClick: () => {
+        setFilteredCharacters('dead');
+        setCurrentPage(1);
+      },
     },
     {
       title: 'Unknown',
       bgColor: '#BFB378',
-      onClick: () => setFilteredCharacters('?status=unknown'),
+      onClick: () => {
+        setFilteredCharacters('unknown');
+        setCurrentPage(1);
+      },
     },
   ];
 
   const getCharacters = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(`https://rickandmortyapi.com/api/character/${filteredCharacters}`);
+      const response = await axios
+        .get(`https://rickandmortyapi.com/api/character/?page=${currentPage.toString()}&status=${filteredCharacters}`);
       setCharacters(response.data.results);
+      setInfo(response.data.info);
+      setPageCount(response.data.info.pages);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         const message = error.response?.status === 404 ? 'Nothing to display' : error.message;
@@ -53,7 +73,9 @@ const CharactersPage = () => {
 
   useEffect(() => {
     getCharacters().then();
-  }, [filteredCharacters]);
+  }, [filteredCharacters, currentPage]);
+
+  const pages = Array.from(Array(pageCount).keys()).map((page) => page + 1);
 
   return (
     <div className="page">
@@ -69,6 +91,36 @@ const CharactersPage = () => {
                   onClick={onClick}
                 />
               ))}
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="box">
+              {info && (
+                <div className="pagination">
+                  {pages.map((page) => (
+                    page === currentPage ? (
+                      <button
+                        key={page}
+                        className="pagination__item"
+                        onClick={() => setCurrentPage(page)}
+                        style={{ outline: '4px solid #BFD962' }}
+                      >
+                        {page}
+                      </button>
+                    ) : (
+                      <button
+                        key={page}
+                        className="pagination__item"
+                        onClick={() => setCurrentPage(page)}
+                      >
+                        {page}
+                      </button>
+                    )
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </div>
